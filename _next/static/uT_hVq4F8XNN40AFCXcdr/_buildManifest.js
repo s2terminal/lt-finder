be1ff3ff9b36a2b275b361d17c0112b1b85c0ddb
s2terminal <s2terminal@users.noreply.github.com>
2021-05-30 06:52:25 +0000

    deploy: 83bd9d72197b39285309f1e6810673f56cf9250b

--- _next/static/uT_hVq4F8XNN40AFCXcdr/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-290c0d62288ba536681c.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-4fdb3ab20c2c63f88fa5.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
